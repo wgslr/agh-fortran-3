2e9b6277758e433dfa3b16624e175663ac2faefc
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-06-22 18:37:02 +0200

    Add sequential array multiplication

---
+++ src/sequential.F90
@@ -0,0 +1,40 @@
+module sequential
+  contains
+  subroutine mm_seq(first, second, multiply, status)
+    real ( kind = 8), intent(in) :: first(:,:) ! pierwsza macierz
+    real ( kind = 8), intent(in) :: second(: ,:) ! druga macierz
+    real ( kind = 8), intent(out) :: multiply(:,:) ! macierz wynikowa
+    integer ( kind = 4), intent(out) :: status ! kod błędu, 0 gdy OK
+    integer ( kind = 4) :: rows1, rows2, cols1, cols2, r, c, i ! kod błędu, 0 gdy OK
+    integer ( kind = 4) :: resultshape(2)
+
+    rows1 = size(first, 1)
+    cols1 = size(first, 2)
+    rows2 = size(second, 1)
+    cols2 = size(second, 2)
+    resultshape = shape(multiply)
+
+    if (cols1 .NE. rows2) then
+      status = 1
+      return
+    end if
+
+    if (ANY((resultshape - (/rows1, cols2/)) /= 0)) then
+      status = 2
+      return
+    end if
+
+    multiply = 0
+
+    do r = 1, rows1
+      do c = 1, cols2
+        do i = 1, cols1
+          multiply(r, c) = multiply(r, c) + first(r, i) * second(i, c)
+        end do
+      end do
+    end do
+
+    status = 0
+  end subroutine
+
+end module
