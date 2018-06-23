b7cf6e713699b707f2560c6a887504f1928f8c67
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-06-23 11:31:03 +0200

    Add sequential Gauss elimination

--- src/sequential.F90
+++ src/sequential.F90
@@ -87,4 +87,25 @@ module sequential
     call mm_seq(first, second, multiply, status)
   end 
 
+
+  subroutine gauss_seq(A, X, n)
+    integer(kind=8), intent(in) :: n
+    real(kind = 8), intent(inout) :: A(0:N, 0:N), X(0:N)
+    real(kind = 8) :: ratio
+    integer(kind=8) :: i, j
+
+    do i = 0, N
+      ! scale row i to have 1 on the diagonal
+      X(i) = X(i) / A(i, i)
+      A(:, i) = A(:, i) / A(i, i)
+      do j = 0, N
+        IF ((i .NE. j) .AND. (A(i, i) .NE. 0)) THEN
+          ratio = A(i, j) / A(i, i)
+          A(:,j) = A(:,j) - ratio * A(:, i)
+          X(j) = X(j) - ratio * x(i)
+        END IF
+      END DO
+    END DO
+  end subroutine
+
 end module
